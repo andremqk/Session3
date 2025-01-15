476667d0a5b44b4d26a9f5aeaf5d289c8a7ea15a
Andre Krepel <dedekrepel@gmail.com>
2025-01-15 18:52:18 +0100

    Added the latest change

---
+++ aritmetic.py
@@ -0,0 +1,7 @@
+print(2+6)
+print(9-99)
+print(3*8)
+print(22/5) #always produces float result (Its probably going to be in the midterm)
+print(22//5) #this is the integer division!
+print(22%5) #this is the "resto" of the division
+print(2**3) #exponential
